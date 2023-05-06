4590871423b8669284a84474303b3330225e12d7
Gerson Lamela <gersonlamela7@gmail.com>
2023-05-06 18:57:27 +0100

    Update Readme

--- README.md
+++ README.md
@@ -1,34 +1,30 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+<p align="center">
+  <img alt="BoraCodar-5-Calculadora" src=".github/assets/Capa.png" />
+</p>
 
-## Getting Started
+## 💻 Projeto
 
-First, run the development server:
+Desafio #BoraCodar#5.
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-```
+## ✨ Tecnologia
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+- [TypeScript](https://www.typescriptlang.org/)
+- [React](https://reactjs.org/)
+- [Tailwindcss](https://tailwindcss.com/)
+- [Nextjs](https://nextjs.org/)
 
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
 
-## Learn More
 
-To learn more about Next.js, take a look at the following resources:
+## 🔖 Layout
 
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
+Podes visualizar o layout do projeto através do link abaixo:
 
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
+- [Layout](https://www.figma.com/community/file/1202607074523509182/%23boraCodar---Desafio-5)
 
-## Deploy on Vercel
+Lembrando que precisas de ter uma conta no [Figma](http://figma.com/).
 
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
 
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+<p align="center">
+  Feito com 💜 by Rocketseat
+</p>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,132 @@
-import Image from 'next/image'
+'use client';
+
+import Image from 'next/image';
+import {useState} from 'react';
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	const [expression, setExpression] = useState('');
+	const [icon, setIcon] = useState('');
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+	const [result, setResult] = useState('');
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+	const handleButtonClick = (value: string) => {
+		switch (value) {
+			case '=':
+				try {
+					setResult(eval(expression));
+					setIcon('../../icons/Equals.svg');
+				} catch (error) {
+					setResult('Error');
+				}
+				break;
+			case 'CE':
+				setExpression('');
+				setResult('');
+				break;
+			case 'C':
+				setExpression((prev) => prev.slice(0, -1));
+				setResult('');
+				break;
+			case '%':
+				const lastNumIndex = expression.search(/[^\d.]*(\d+.?\d*)$/);
+				const lastNum = expression.slice(lastNumIndex);
+				const percentage = (parseFloat(lastNum) / 100).toString();
+				setExpression((prev) => prev.slice(0, lastNumIndex) + percentage);
+				setIcon('../../icons/Percent.svg');
+				break;
+			case '+/-':
+				const lastOperatorIndex = expression.search(/[\+\-\*\/]+[^\.]*$/);
+				const lastNumIndex1 = expression.search(/[^\d.]*(\d+.?\d*)$/);
+				const lastNum1 = expression.slice(lastNumIndex1);
+				const sign = lastNum1.startsWith('-') ? '' : '-';
+				setExpression((prev) => prev.slice(0, lastOperatorIndex + 1) + sign + lastNum1.slice(1));
+				setIcon('../../icons/PlusMinus.svg');
+				break;
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+			default:
+				setExpression((prev) => prev + value);
+				break;
+		}
+	};
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-center bg-[#807ECE]">
+			<div className="flex flex-col w-[356px] bg-[#2D2A37] pt-[54px] pb-[32px] px-[37px] rounded-[48px] gap-[26px]  shadow-body-calculator">
+				<div className="w-full pl-[18px] pr-[22px] flex flex-col items-end justify-between gap-2">
+					<span className="text-[#6B6B6B] text-xl"> {expression || '0'}</span>
+					<div className="flex items-center justify-between w-full ">
+						<Image src={icon || '../../icons/Equals.svg'} alt="Equals" width={28} height={28} />
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+						<span className="text-4xl text-[#EBEBEB]"> {result || '0'}</span>
+					</div>
+				</div>
+
+				<div className="grid grid-cols-4 gap-3">
+					<button onClick={() => handleButtonClick('CE')} className="w-16 h-16 bg-[#2D2A37] shadow-button-calculator rounded-full flex items-center justify-center ">
+						<span className="text-[#975DFA] text-2xl">CE</span>
+					</button>
+
+					<button onClick={() => handleButtonClick('C')} className="w-16 h-16 bg-[#2D2A37] shadow-button-calculator rounded-full flex items-center justify-center ">
+						<span className="text-2xl text-[#EBEBEB]">C</span>
+					</button>
+
+					<button onClick={() => (handleButtonClick('%'), setIcon('../../icons/Percent.svg'))} className="w-16 h-16 bg-[#2D2A37] shadow-button-calculator rounded-full flex items-center justify-center ">
+						<Image src="../../icons/Percent.svg" alt="percent" width={28} height={28} />
+					</button>
+
+					<button onClick={() => (handleButtonClick('/'), setIcon('../../icons/Divide.svg'))} className="w-16 h-16 bg-[#462878] shadow-button-calculator rounded-full flex items-center justify-center ">
+						<Image src="../../icons/Divide.svg" alt="Divide" width={28} height={28} />
+					</button>
+					<button onClick={() => handleButtonClick('7')} className="w-16 h-16 bg-[#2D2A37] shadow-button-calculator rounded-full flex items-center justify-center ">
+						<span className="text-2xl text-[#EBEBEB]">7</span>
+					</button>
+					<button onClick={() => handleButtonClick('8')} className="w-16 h-16 bg-[#2D2A37] shadow-button-calculator rounded-full flex items-center justify-center ">
+						<span className="text-2xl text-[#EBEBEB]">8</span>
+					</button>
+					<button onClick={() => handleButtonClick('9')} className="w-16 h-16 bg-[#2D2A37] shadow-button-calculator rounded-full flex items-center justify-center ">
+						<span className="text-2xl text-[#EBEBEB]">9</span>
+					</button>
+					<button onClick={() => (handleButtonClick('*'), setIcon('../../icons/X.svg'))} className="w-16 h-16 bg-[#462878] shadow-button-calculator rounded-full flex items-center justify-center ">
+						<Image src="../../icons/X.svg" alt="X" width={28} height={28} />
+					</button>
+					<button onClick={() => handleButtonClick('4')} className="w-16 h-16 bg-[#2D2A37] shadow-button-calculator rounded-full flex items-center justify-center ">
+						<span className="text-2xl text-[#EBEBEB]">4</span>
+					</button>
+					<button onClick={() => handleButtonClick('5')} className="w-16 h-16 bg-[#2D2A37] shadow-button-calculator rounded-full flex items-center justify-center ">
+						<span className="text-2xl text-[#EBEBEB]">5</span>
+					</button>
+					<button onClick={() => handleButtonClick('6')} className="w-16 h-16 bg-[#2D2A37] shadow-button-calculator rounded-full flex items-center justify-center ">
+						<span className="text-2xl text-[#EBEBEB]">6</span>
+					</button>
+					<button onClick={() => (handleButtonClick('-'), setIcon('../../icons/Minus.svg'))} className="w-16 h-16 bg-[#462878] shadow-button-calculator rounded-full flex items-center justify-center ">
+						<Image src="../../icons/Minus.svg" alt="Minus" width={28} height={28} />
+					</button>
+					<button onClick={() => handleButtonClick('1')} className="w-16 h-16 bg-[#2D2A37] shadow-button-calculator rounded-full flex items-center justify-center ">
+						<span className="text-2xl text-[#EBEBEB]">1</span>
+					</button>
+					<button onClick={() => handleButtonClick('2')} className="w-16 h-16 bg-[#2D2A37] shadow-button-calculator rounded-full flex items-center justify-center ">
+						<span className="text-2xl text-[#EBEBEB]">2</span>
+					</button>
+					<button onClick={() => handleButtonClick('3')} className="w-16 h-16 bg-[#2D2A37] shadow-button-calculator rounded-full flex items-center justify-center ">
+						<span className="text-2xl text-[#EBEBEB]">3</span>
+					</button>
+					<button onClick={() => (handleButtonClick('+'), setIcon('../../icons/Plus.svg'))} className="w-16 h-16 bg-[#462878] shadow-button-calculator rounded-full flex items-center justify-center ">
+						<Image src="../../icons/Plus.svg" alt="Plus" width={28} height={28} />
+					</button>
+					<button onClick={() => (handleButtonClick('+/-'), setIcon('../../icons/PlusMinus.svg'))} className="w-16 h-16 bg-[#2D2A37] shadow-button-calculator rounded-full flex items-center justify-center ">
+						<Image src="../../icons/PlusMinus.svg" alt="PlusMinus" width={28} height={28} />
+					</button>
+					<button onClick={() => handleButtonClick('0')} className="w-16 h-16 bg-[#2D2A37] shadow-button-calculator rounded-full flex items-center justify-center ">
+						<span className="text-2xl text-[#EBEBEB]">0</span>
+					</button>
+					<button onClick={() => handleButtonClick('.')} className="w-16 h-16 bg-[#2D2A37] shadow-button-calculator rounded-full flex items-center justify-center ">
+						<span className="text-2xl text-[#EBEBEB]">,</span>
+					</button>
+					<button onClick={() => (handleButtonClick('='), setIcon('../../icons/Equals.svg'))} className="w-16 h-16 bg-[#462878] shadow-button-calculator rounded-full flex items-center justify-center ">
+						<Image src="../../icons/Equals.svg" alt="Equals" width={28} height={28} />
+					</button>
+				</div>
+			</div>
+		</main>
+	);
 }
